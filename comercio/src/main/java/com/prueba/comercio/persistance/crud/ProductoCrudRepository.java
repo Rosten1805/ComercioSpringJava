db6032c803c7fe1589bb71dd616f400abc8a1e74
Cristina Cañadas Gómez - Limón <123247001+Rosten1805@users.noreply.github.com>
2023-11-24 17:13:15 +0100

    Add files via upload

---
+++ comercio/src/main/java/com/prueba/comercio/persistance/crud/ProductoCrudRepository.java
@@ -0,0 +1,12 @@
+package com.prueba.comercio.persistance.crud;
+
+import com.prueba.comercio.persistance.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
